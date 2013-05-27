c73a32af7246c34ca12a8b1190bcb05a5633efdc
Denis Rouzaud <denis.rouzaud@gmail.com>
2013-05-27 15:16:42 +0200

    LICENCE

--- types/string.py
+++ types/string.py
@@ -1,9 +1,32 @@
-"""
-Custom settings for QGIS plugin
-Denis Rouzaud
-denis.rouzaud@gmail.com
-Feb. 2013
-"""
+#-----------------------------------------------------------
+#
+# QGIS setting manager is a python module to easily manage read/write
+# settings and set/get corresponding widgets.
+#
+# Copyright    : (C) 2013 Denis Rouzaud
+# Email        : denis.rouzaud@gmail.com
+#
+#-----------------------------------------------------------
+#
+# licensed under the terms of GNU GPL 2
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this progsram; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#---------------------------------------------------------------------
+
+
 # options:
 # comboMode: can be data or text. It defines if setting is found directly in combobox text or rather in the userData.
 
