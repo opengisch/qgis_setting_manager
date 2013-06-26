6092fead7554f5d9466c3e42a13b16c00dd5ee40
Denis Rouzaud <denis.rouzaud@gmail.com>
2013-06-26 16:27:43 +0200

    fix error message

--- types/bool.py
+++ types/bool.py
@@ -55,6 +55,6 @@ class Bool(Setting):
             self.widgetSetMethod = widget.setChecked
             self.widgetGetMethod = widget.isChecked
         else:
-            raise NameError("SettingManager does not handle %s widgets for integers for the moment (setting: %s)" %
+            raise NameError("SettingManager does not handle %s widgets for booleans for the moment (setting: %s)" %
                             (type(widget), self.name))
         self.widget = widget
